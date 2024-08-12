32374ced3cd384b14299a0316d46c3ec7a8c217e
kseran <mr.lord.moon@mail.ru>
2024-08-12 16:35:44 +0300

    added comments to functions and some other changes

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Table, Column, Integer, String
-from database import metadata
+from database import ContDateBase
 
 users = Table(
     "users",
-    metadata,
+    ContDateBase.get_metadata(),
     Column("id", Integer, primary_key=True, index=True),
     Column("username", String, unique=True, index=True),
     Column("hashed_password", String),
